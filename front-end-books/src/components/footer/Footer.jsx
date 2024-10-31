0f886478f8251d4b684670f5871d477b7b0f0a4e
haipham2004 <bophamnb2004@gmail.com>
2024-10-31 15:31:33 +0700

    haphamyka_31_10_2024

--- front-end-books/src/components/footer/Footer.jsx
+++ front-end-books/src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
-const Footer = () =>{
-    return(
-        <>
-        footer
-        </>
-    )
+import './FooterCss.css';
+
+const Footer = () => {
+    return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <h5>Về Chúng Tôi</h5>
+                        <p>Cửa hàng sách XYZ cung cấp những cuốn sách hay nhất cho mọi lứa tuổi.</p>
+                    </div>
+                    <div className="col-md-4">
+                        <h5>Liên Hệ</h5>
+                        <ul style={{ listStyleType: 'none', padding: 0 }}>
+                            <li>Email: phamngochai3010yka@gmail.com</li>
+                            <li>Điện thoại: +84 334 294 889</li>
+                            <li>Địa chỉ: 10 Trương Công Giai, Cầu Giấy, Hà Nội</li>
+                        </ul>
+                    </div>
+                    <div className="col-md-4">
+                        <h5>Kết Nối Với Chúng Tôi</h5>
+                        <a href="#" style={{ margin: '0 10px' }}>Facebook</a>
+                        <a href="#" style={{ margin: '0 10px' }}>Instagram</a>
+                        <a href="#" style={{ margin: '0 10px' }}>Twitter</a>
+                    </div>
+                </div>
+                <hr />
+                <p>&copy; 2024 Cửa hàng sách XYZ. Bản quyền thuộc về chúng tôi.</p>
+            </div>
+    );
 }
+
 
 export default Footer
--- front-end-books/src/components/header/Header.jsx
+++ front-end-books/src/components/header/Header.jsx
@@ -1,7 +1,73 @@
-const Header = () =>{
-    return(
+import {
+    HomeOutlined,
+    UserOutlined,
+    ShopOutlined,
+    HeartOutlined,
+    ShoppingCartOutlined,
+    SearchOutlined,
+} from '@ant-design/icons';
+import './HeaderCss.css';
+
+
+const Header = () => {
+    return (
         <>
-        header
+            <div className="banner">
+                <div className="logo">
+                    <div className="icon">
+                        <HeartOutlined style={{ fontSize: '32px', color: '#ff4d4d' }} />
+                    </div>
+                    <div className="text">phamngochai3010yka</div>
+                </div>
+                <div className="nav">
+                    <ul>
+                        <li>
+                            <a href="/">
+                                <HomeOutlined />
+                                <span>Home</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/about">
+                                <span>About</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/shop">
+                                <ShopOutlined />
+                                <span>Shop</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/blogs">
+                                <span>Blogs</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/pages">
+                                <span>Pages</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/contact">
+                                <span>Contact</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="login">
+                                <span>Contact</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="actions">
+                    <SearchOutlined />
+                    <UserOutlined />
+                    <HeartOutlined />
+                    <ShoppingCartOutlined />
+                    <span>(02)</span>
+                </div>
+            </div>
         </>
     )
 }
