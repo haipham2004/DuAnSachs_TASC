50dcd252cc3925d72b246828e72a4c5c968831e6
haipham2004 <bophamnb2004@gmail.com>
2024-12-03 16:19:00 +0700

    hh321

--- back-end/books-service/src/main/java/com/example/books_service/config/RedisConfig.java
+++ back-end/books-service/src/main/java/com/example/books_service/config/RedisConfig.java
@@ -1,60 +1,56 @@
-//package com.example.books_service.config;
-//
-//import org.springframework.cache.CacheManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.cache.RedisCacheConfiguration;
-//import org.springframework.data.redis.cache.RedisCacheManager;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.RedisSerializationContext;
-//import org.springframework.data.redis.serializer.RedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//import java.time.Duration;
-//
-//@Configuration
-//public class RedisConfig {
-//
-//    @Bean
-//    public RedisConnectionFactory redisConnectionFactory() {
-//        return new LettuceConnectionFactory("localhost", 6379); // Tùy chỉnh host và port
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(redisConnectionFactory);
-//
-//        // Cấu hình serializer
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        template.setHashKeySerializer(new StringRedisSerializer());
-//        template.setHashValueSerializer(new StringRedisSerializer());
-//
-//        return template;
-//    }
-//
-////    @Bean
-////    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-////        // Sử dụng RedisSerializationContext để cấu hình key và value serializer
-////        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
-////                .entryTtl(Duration.ofMinutes(10))  // TTL cho cache (10 phút)
-////                .disableCachingNullValues()
-////                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))  // Sử dụng RedisSerializer.string() cho key
-////                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()));  // Sử dụng RedisSerializer.string() cho value
-////
-////        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
-////                .cacheDefaults(cacheConfig)
-////                .build();
-////    }
-//
+package com.example.books_service.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379); // Tùy chỉnh host và port
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Cấu hình serializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        return template;
+    }
+
 //    @Bean
 //    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-//        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig();
-//        return RedisCacheManager.builder(redisConnectionFactory)
+//        // Sử dụng RedisSerializationContext để cấu hình key và value serializer
+//        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+//                .entryTtl(Duration.ofMinutes(10))  // TTL cho cache (10 phút)
+//                .disableCachingNullValues()
+//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()))  // Sử dụng RedisSerializer.string() cho key
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.string()));  // Sử dụng RedisSerializer.string() cho value
+//
+//        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
 //                .cacheDefaults(cacheConfig)
 //                .build();
 //    }
-//}
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+}
